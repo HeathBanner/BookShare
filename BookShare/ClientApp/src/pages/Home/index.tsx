a895a4dcccf6d98812b8ff82020bffcf5fd66296
HeathBanner <heathbanner@outlook.com>
2020-08-13 11:45:40 -0400

    Updated due to .tsx conversion

---
+++ BookShare/ClientApp/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+import Landing from '../../components/Home/Landing';
+
+import { Grid } from '@material-ui/core';
+
+export default () => <Grid container><Landing /></Grid>;

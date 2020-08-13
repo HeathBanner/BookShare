fdb20157f98618fe637b69703a955af6fc2fcff3
HeathBanner <heathbanner@outlook.com>
2020-08-13 11:42:09 -0400

    Changing from .js to .tsx

--- BookShare/ClientApp/src/pages/Home/index.js
+++
@@ -1,7 +0,0 @@
-﻿import React from 'react';
-
-import Landing from '../../components/Home/Landing';
-
-import { Grid } from '@material-ui/core';
-
-export default () => <Grid container><Landing /></Grid>;

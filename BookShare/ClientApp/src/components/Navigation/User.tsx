6018496f4c332b3a852be9fae8289c1be8819bb2
HeathBanner <heathbanner@outlook.com>
2020-08-19 14:46:18 -0400

    Changed from .js to .tsx

---
+++ BookShare/ClientApp/src/components/Navigation/User.tsx
@@ -0,0 +1,164 @@
+﻿import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { History, LocationState } from 'history';
+import { withRouter } from 'react-router-dom';
+
+import { withStyles, createStyles } from '@material-ui/styles';
+import {
+    Avatar,
+    Button,
+    Popper,
+    Fade,
+    Paper,
+    Typography,
+    ClickAwayListener
+} from '@material-ui/core';
+
+const styles = () => createStyles({
+    popper: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '100%'
+    },
+    button: {
+        position: 'absolute',
+        right: 16,
+        top: '50%',
+        transform: 'translate(0%, -50%)',
+        padding: 12
+    },
+    avatar: {
+        backgroundColor: 'red'
+    },
+    paper: {
+        width: '20%',
+        padding: 10
+    },
+    buttons: {
+        width: '100%',
+        padding: 10,
+        whiteSpace: 'nowrap'
+    }
+});
+
+const buttons = [
+    {
+        text: "Profile",
+        url: "/profile",
+        func: false
+    },
+    {
+        text: "My Books",
+        url: "/bookshelf",
+        func: false
+    },
+    {
+        text: "Signout",
+        func: true
+    }
+];
+
+interface IProps extends RouteComponentProps {
+    user : any;
+    history : History<LocationState>;
+    classes : any;
+    dispatch : any;
+};
+
+interface IState {
+    anchorEl : any;
+};
+
+class User extends Component<IProps, IState> {
+
+    constructor(props : IProps) {
+        super(props);
+        this.state = {
+            anchorEl: null
+        };
+    };
+
+    handleClick = (event : React.MouseEvent<HTMLButtonElement>) : void => {
+        try {
+            const selection = event.currentTarget;
+            const getBoundingClientRect = () => selection.getBoundingClientRect();
+    
+            if (this.state.anchorEl) {
+                return this.setState({ anchorEl: null });
+            }
+    
+            this.setState({
+                anchorEl: {
+                    clientWidth: getBoundingClientRect().width,
+                    clientHeight: getBoundingClientRect().height,
+                    getBoundingClientRect
+                }
+            });
+        } catch (error) {
+            this.props.dispatch({ type: "ERROR_NOTIFY", payload: "Something went wrong :(" });
+        }
+    };
+    closeAnchor = () => this.setState({ anchorEl: null });
+
+    open : boolean = Boolean(this.state.anchorEl);
+    id : string | undefined = this.open ? 'transition-popper' : undefined;
+
+    signOut = () => this.props.dispatch({ type: "SIGNOUT" });
+
+    handleHistory(url : string) : void {
+        try {
+            this.props.history.push(url);
+            this.setState({ anchorEl: null });
+        } catch (error) {
+            this.props.dispatch({ type: "ERROR_NOTIFY", payload: "Something went wrong :(" });
+        }
+    };
+
+    render() {
+        return (
+            <>
+                <Button
+                    className={this.props.classes.button}
+                    onClick={this.handleClick}
+                >
+                    <Avatar className={this.props.classes.avatar}>
+                        {this.props.user.username.charAt(0)}
+                    </Avatar>
+                </Button>
+    
+                <Popper
+                    className={this.props.classes.popper}
+                    id={this.id}
+                    open={this.open}
+                    anchorEl={this.state.anchorEl}
+                    transition
+                >
+                    {({ TransitionProps }) => (
+                        <Fade {...TransitionProps} timeout={50}>
+                            <ClickAwayListener onClickAway={this.closeAnchor}>
+                                <Paper className={this.props.classes.paper}>
+                                    {buttons.map((item : any) => {
+                                        return (
+                                            <Button
+                                                key={item.text}
+                                                className={this.props.classes.buttons}
+                                                onClick={() => item.func ? this.signOut() : this.handleHistory(item.url)}
+                                            >
+                                                <Typography variant="body2">
+                                                    {item.text}
+                                                </Typography>
+                                            </Button>
+                                        );
+                                    })}
+                                </Paper>
+                            </ClickAwayListener>
+                        </Fade>
+                    )}
+                </Popper>
+             </>
+        );
+    };
+};
+
+export default connect()(withStyles(styles)(withRouter(User)));
